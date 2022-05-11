0fb65527ac62713234da01f53ffe98516d95526a
Alfredo Sánchez <alfred3ks@gmail.com>
2022-05-11 18:00:33 +0200

    Como usar fetch() para conectarnos a una API externa.

--- json_course/index.js
+++ json_course/index.js
@@ -1,127 +1,101 @@
-/*
-Aqui tenemos un objeto del lenguaje JS: Un tipo de dato del lenguaje, puedes ver que se puede definir ese objeto de dos formas metiendo el valor entre dobles comillas o no.
+/* Objetos de JS y Objetos de JSON
 
-const user1 = {
-    "name": "Rayn",
-    "lastname": "Ray",
-    "age": 25,
-    "nickname": "Rayn123",
-    "hobbies":["run", "code", "eat"],
+Curso practico de JSON - JavaScript Object Notation.
+
+---¿Porque aprender JSON?
+
+- Formato popular para el intercambio de datos entre diferentes sistemas sin importar el lenguaje de programacion.
+- Bastante utilizado en REST APIs.
+- Es usado en Bases de datos.
+
+---¿Que es JSON?
+
+Abreviado de JavaScript Object Notation. Es un formato ligero de intercambio de datos. Formato basado en JavaScript pero puede ser usado por cualquier lenguaje de programacion. Facil de escribir. Usado en el mundo web como AJAX.
+
+---Tipos de datos de JSON: Porque como un lenguaje de programacion este formato tambien los tiene:
+
+- numbers: Integer y numeros flotantes
+- string: Caracteres Unicode, que colocaremos con dobles comillas "".
+- boolean: True o False, representa los dos estados.
+- arrays: Listas ordenadas que comienzan con sus indeice en cero.
+- objetos: Datos representados por pares de clave - valor.
+    {"nickname": "Alfred"}
+- null: Para datos no disponibles o que no existe aun.
+
+---Reglas para poder crear un archivo json:
+
+- Pares clave:valor, JSON esta conformado por datos en clave/valor:
+    name: "eugenia",
+    password: "eugenia123",
+    country: "spain",
+    age: 27
+
+Representaqcion de datos facil de leer y entender.
+
+- Las claves deben ir en doble comillas tambien: Si o si debe ser asi:
+    "name": "eugenia",
+    "password": "eugenia123",
+    "country": "spain",
+    "age": 27
+
+- Cualquier clave que creemos siempre, siempre debe tener un valor. Podemos crear una clave con un valor vacio:
+    "nickname": ""
+
+- Colocar todos estos clave/valor entre llaves: asi agrupamos todos los datos en una sola entidad.
+{
+    "name": "eugenia",
+    "password": "eugenia123",
+    "country": "spain",
+    "age": 27
+}
+
+- Para separar los claves valor unos de otros debemos usar coma, "," salvo el ultimo par clave valor que no llevara.
+
+- Podemos anidar objetos, uno dentro de otros:
+
+{
+    "name": "eugenia",
+    "password": "eugenia123",
     "address": {
-        "street": "123 Main St",
-        "city": "New York"
+        "city": "london",
+        "street": "baker street 123"
     },
-    "married": true
+    "age": 27
 }
 
-const user2 = {
-    name: "Jose",
-    lastname: "Perez"
-}
+- Las extensiones de los archivos deben ser .json:
 
-console.log(user1);
-console.log(user2);
+    datos.json
 
-Para que ese objeto de JS sea un objeto JSON debemos transformarlo, y para eso el lengueje de JS nos aporta un metodo JSON.stringify() recibe por parametro el objeto a convertir.
+    mydata.json
 
-const json1 = JSON.stringify(user1);
-const json2 = JSON.stringify(user2);
-console.log(json1);
-console.log(json2);
+    users.json
 
-Como sabemos que tenemos un objeto en JSON, existe una herramienta online que podemos usar para comprobarlo, la encontramos en el siguiente enlace: json validator.
+- Cuando enviamos datos de un sistema a otro por lo general le damos informacion al otro sistema que datos le estamos enviando, eso se conoce como mime type, o el tipo de datos de archivos que estoy enviando, en json es comun usar:
 
-Pero que pasa ahora si vemos que los objetos de JS tienen unos tipos que el objeto JSON no tiene como por ejemplo una funcion vamos a agregarle a nuesto objeto un metodo saludar().
+    application/json
 
-const user1 = {
-    "name": "Rayn",
-    "lastname": "Ray",
-    "age": 25,
-    "nickname": "Rayn123",
-    "hobbies":["run", "code", "eat"],
-    "address": {
-        "street": "123 Main St",
-        "city": "New York"
-    },
-    "married": true,
-    greet(){
-        return "Hola"
-    }
-}
-
-Si analizamos la transformacion vemos que al ser un tipo de datos que los objetos JSON no lo reconoce y simplemente no lo transforma.
-
-Hasta el momento hemos visto un solo usuario pero suponiendo que tenemos una lista de usuarios, suponiendo que tenemos una lista de objeto, pra eso tenemos los corchetes []. Dentro tendremos cada objeto separados por comas:
-
-const friends = [
-    {},
-    {},
-    {},
-    {}
-]
-
-const friends = [
-    {
-        name: "Joe",
-        nickname: "Joe123"
-    },
-    {
-        name: "Martin",
-        nickname: "Martin123"
-    },
-    {
-        name: "Julliet",
-        nickname: "Julliet123"
-    },
-    {
-        name: "Dan",
-        nickname: "Dan123"
-    }
-]
-
-const friendsJSON = JSON.stringify(friends);
-console.log(friendsJSON);
-
-
-Ahora podemos agregar al primer objeto user1 la lista de amigos:
-
-user1.friends = friends;
-const json2 = JSON.stringify(user2);
-console.log(json1);
-
-Ahora vamos a ver como mostrar esa lista de amigos, suponiendo que esa lista nos viene de un servidor y lo vamos a mostrar en el HTML
-
-Aqui metenos la lista de amigos en HTML recorremos el array con un for:
-let output = '';
-for (let i= 0; i < friends.length; i++) {
-    output = output + `<li>${friends[i].name} - ${friends[i].nickname}</li>`;
-    console.log(output);
-}
-document.getElementById('people').innerHTML = output;
-
-El metodo para convertir unJSON que nos viene del sevidor es el metodo JSON.parse() y recibe por parametro el objeto JSON.
-
-const userObje = JSON.parse(json1);
-console.log(userObje);
-
-Lo tenemos que transformar a objeto de JS para poder usar todo el potencial de JS.
 */
 
+// Tenemos nuestro primer objeto de JS:
+
 const user1 = {
     "name": "Rayn",
     "lastname": "Ray",
     "age": 25,
     "nickname": "Rayn123",
-    "hobbies":["run", "code", "eat"],
+    "hobbies": ["run", "code", "eat"],
     "address": {
         "street": "123 Main St",
         "city": "New York"
     },
     "married": true,
-    greet(){
+    greet() {
         return "Hola"
     }
 }
+
+// Tenemos un segundo objeto de JS vemos que no usa la clave doble comillas, eso es permitido en los objetos de JS:
 
 const user2 = {
     name: "Jose",
@@ -131,10 +105,21 @@ const user2 = {
 // console.log(user1);
 // console.log(user2);
 
+// Transformamos eso Objetos de JS a Objetos de JSON usando el metodo JSON.stringify() recibe por parametro el objeto de JS:
+
 const json1 = JSON.stringify(user1);
 const json2 = JSON.stringify(user2);
 // console.log(json1);
 // console.log(json2);
+
+/*Creamos un objeto de amigos, con son varios creamos un array usando los corchete []:
+const friends = [
+    {},
+    {},
+    {},
+    {}
+]
+*/
 
 const friends = [
     {
@@ -155,23 +140,141 @@ const friends = [
     }
 ]
 
+// Agregamos al objeto user1 la lista de amigos:
 user1.friends = friends;
 
+// Pasamos la lista de amigos de Objeto JS a Objeto JSON:
 const friendsJSON = JSON.stringify(friends);
 // console.log(friendsJSON);
 // console.log(user1);
 
-// Aqui metenos la lista de amigos en HTML recorremos el array con un for:
+// Aqui vemos como mostrar la lista de amigos en HTML recorremos el array con un for para mostrarla en el dom del archivo index.html:
 let output = '';
-for (let i= 0; i < friends.length; i++) {
+for (let i = 0; i < friends.length; i++) {
     output = output + `<li>${friends[i].name} - ${friends[i].nickname}</li>`;
     console.log(output);
 }
 document.getElementById('people').innerHTML = output;
 
-// El metodo para convertir unJSON que nos viene del sevidor es el metodo JSON.parse() y recibe por parametro el objeto JSON.
-
+// El metodo para convertir un JSON que nos viene del servidor es el metodo JSON.parse() y recibe por parametro el objeto JSON.
 const userObj = JSON.parse(json1);
 console.log(userObj);
 
+/*
+Ahora vamos a meter dentro de un archivo .JSON nuestro user1. Vemos el archivo que se llama user.JSON
+*/
 
+    console.log(JSON.stringify(user1));
+
+/*
+Tambien podemos ver un archivo JSON en el navegador basta con arrastrar ese archivo al navegador y este tiene la capacidad de poder mostrarlo.
+
+Ahora vamos a ver como consumir datos desde el servidor, esos datos nos vendran en JSON, no nos vamos a conectar haremos un servidor en local y este nos servira esos datos, lo haremos con NodeJS.
+
+El primer programa que instaleremos sera liveserver. Paa tener un servidor web: Lo haremos todo sobre la carpeta json_course:
+    npm install -g live-server
+
+Lo instalamos de manera global. ahora vamos a correr la app usando ese servidor de liveserver: Usaremos la terminal de VScode.
+Simplemeente sobre la carpeta damos live-server y nos abria un servidor y vemos como corre nuestra app en el navegador.
+
+Ahora este servidor sera el encargado de servirnos el archivo user.json
+
+http://127.0.0.1:8080/user.json
+
+// 20220511163050
+// http://127.0.0.1:8080/user.json
+
+{
+    "name": "Rayn",
+    "lastname": "Ray",
+    "age": 25,
+    "nickname": "Rayn123",
+    "hobbies": [
+        "run",
+        "code",
+        "eat"
+    ],
+    "address": {
+        "street": "123 Main St",
+        "city": "New York"
+    },
+    "married": true,
+    "friends": [
+        {
+        "name": "Joe",
+        "nickname": "Joe123"
+        },
+        {
+        "name": "Martin",
+        "nickname": "Martin123"
+        },
+        {
+        "name": "Julliet",
+        "nickname": "Julliet123"
+        },
+        {
+        "name": "Dan",
+        "nickname": "Dan123"
+        }
+    ]
+}
+
+Esos son los datos que nos esta mandando el servidor en local a traves del protocolo http.
+
+Tambien existen redes APIS donde podemos obtener datos:
+
+Tenemos JSONPlaceholder:
+https://jsonplaceholder.typicode.com/
+
+Tenemos varios archivos a los cuales acceder por ejemplo post o user.
+
+https://jsonplaceholder.typicode.com/posts
+
+https://jsonplaceholder.typicode.com/users
+
+Ahora vamos a usar nuestro archivo user.json como si viniera de un servidor externo.
+
+Para traer esos datos el lenguaje JS nos prevee de un metodo llamado fetch() recibe  por parametro la url del servidor y asi podemos traer datos desde otro servidor: Lo vemos a continuacion:
+
+*/
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((resp)=>{
+        return resp.json();
+    })
+    .then(data =>{
+        console.log(data);
+    })
+
+// Ahora ua vez que tenemos los datos los podemos recorrer como hicimos arriba y mostrarlos en el dom:
+
+let postsElements = '';
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((resp) => {
+    return resp.json();
+})
+.then(data => {
+    for(let i = 0; i<data.length; i++){
+        postsElements += `<li>${data[i].userId} - ${data[i].title}</li>`
+    }
+    document.getElementById('posts').innerHTML = postsElements;
+})
+
+/*
+Ahora vamos a ver como hacerlo con nuestro arreglo de amigos que es lo mismo que hemos visto anterior pero eso viene de otro servidor
+*/
+
+let amigos = '';
+fetch('/user.JSON')
+    .then(respuesta =>{
+        return respuesta.json();
+    })
+    .then(data=>{
+        console.log(data.friends);
+        for (let i = 0; i < data.friends.length; i++) {
+            amigos += `<li>${data.friends[i].name} - ${data.friends[i].nickname
+}</li>`
+        }
+        document.getElementById('friends').innerHTML = amigos;
+
+    })
